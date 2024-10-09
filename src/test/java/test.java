919dec6949eb16cf609342ae0cb710afbf02cde3
UAArthur <merklingerarthur@gmail.com>
2024-10-09 02:11:38 +0200

    - filled README.md - Added code to test.java

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -6,6 +6,7 @@ import net.hauntedstudio.response.auth.untis.UntisSchoolResponse;
 
 public class test {
     public static void main(String[] args) {
-
+        SchoolFox schoolFox = new SchoolFox();
+        schoolFox.login("username", "password");
     }
 }
